d08dd71886041f609e5aa15ca4c50aa64bfd963b
protomouse <root@protomou.se>
2016-04-07 14:05:49 +0200

    coalesce filter

---
+++ filters/coalesce.js
@@ -0,0 +1,91 @@
+'use strict';
+
+let OnigRegExp = require('oniguruma').OnigRegExp
+  , _ = require('highland')
+  , Grok = require('../lib/grok').Grok;
+
+function key(event) {
+  return [event.type, event.host, event.source].map(function(s) { return s || ''; }).join('-');
+}
+
+module.exports = function(remaining, opts) {
+  opts = opts || {};
+
+  let grok = new Grok;
+  grok.loadDefaultSync();
+  if (opts.patternsDir) {
+    grok.loadSync(path.join(opts.patternsDir, opts.patternsFilesGlob || '*'));
+  }
+
+  let buf = {}
+    , pattern = grok.createPattern(opts.pattern)
+    , regexp = new OnigRegExp(pattern.resolved)
+    , negate = !!opts.negate
+    , what = opts.what || 'previous'
+    , maxEvents = opts.maxEvents || 500
+    , source = opts.source || 'message'
+    , handler;
+
+  function buffer(key, event) {
+    buf[key] = buf[key] || [];
+    buf[key].push(event);
+  }
+
+  function bufferFull(key) {
+    return buf[key] && buf[key].length >= maxEvents;
+  }
+
+  function bufferEmpty(key) {
+    return !buf[key] || buf[key].length === 0;
+  }
+
+  function merge(key) {
+    let events = buf[key]
+      , event = events.reduce(function(dst, src) {
+        return _.extend(dst, _.extend(src, {}));
+      });
+
+    event[source] = events.map(function(e) { return e[source]; }).filter(function(v) { return v !== undefined; }).join('\n');
+    return event;
+  }
+
+  if (what === 'previous') {
+    handler = function(key, event, matches, push) {
+      if ((!matches || bufferFull(key)) && !bufferEmpty(key)) {
+        push(null, merge(key));
+        delete buf[key];
+      }
+
+      buffer(key, event);
+    };
+  } else if (what === 'next') {
+    handler = function(key, event, matches, push) {
+      buffer(key, event);
+
+      if (!matches || bufferFull(key)) {
+        push(null, merge(key));
+        delete buf[key];
+      }
+    };
+  } else {
+    throw new Error('what must equal previous or next');
+  }
+
+  return _.pipeline(function(events) {
+    return events.consume(function(err, event, push, next) {
+      if (err) {
+        push(err);
+        next();
+      } else if (event === _.nil) {
+        _.values(buf).pipe(remaining);
+        push(null, _.nil);
+      } else {
+        regexp.test(event[source], function(err, matches) {
+          matches = (matches && !negate) || (!matches && negate);
+          handler(key(event), event, matches, push);
+          next();
+        });
+      }
+    });
+  });
+};
---
+++ test/unit/filters/coalesce_test.js
@@ -0,0 +1,169 @@
+'use strict';
+
+let expect = require('chai').expect
+  , _ = require('highland')
+  , coalesce = require('../../../filters/coalesce');
+
+describe('filters/coalesce', function() {
+  it('transforms multiple events into one', function(done) {
+    let remaining = _();
+
+    _([
+      {seq: '1', type: 'logs', host: 'foo', source: 'bar', message: 'one'},
+      {seq: '2', type: 'logs', host: 'foo', source: 'bar', message: '  two'},
+      {seq: '3', type: 'logs', host: 'foo', source: 'bar', message: '  three'},
+      {seq: '4', type: 'logs', host: 'foo', source: 'bar', message: 'four'}
+    ])
+    .pipe(coalesce(remaining, {
+      pattern: '^\\s+'
+    }))
+    .toArray(function(events) {
+      expect(events).to.eql([
+        {
+          seq: '1',
+          type: 'logs',
+          host: 'foo',
+          source: 'bar',
+          message: 'one\n  two\n  three'
+        }
+      ]);
+
+      remaining.toArray(function(events) {
+        expect(events).to.eql([
+          [{
+            seq: '4',
+            type: 'logs',
+            host: 'foo',
+            source: 'bar',
+            message: 'four'
+          }]
+        ]);
+
+        done();
+      });
+    });
+  });
+
+  it('supports grok patterns', function(done) {
+    let remaining = _();
+
+    _([
+      {seq: '1', type: 'logs', host: 'foo', source: 'bar', message: '[2016-04-07T07:38:04.396Z] one'},
+      {seq: '2', type: 'logs', host: 'foo', source: 'bar', message: 'two'},
+      {seq: '3', type: 'logs', host: 'foo', source: 'bar', message: 'three'},
+      {seq: '4', type: 'logs', host: 'foo', source: 'bar', message: '[2016-04-07T07:38:05.396Z] four'}
+    ])
+    .pipe(coalesce(remaining, {
+      pattern: '\\[%{TIMESTAMP_ISO8601}\\]',
+      negate: true
+    }))
+    .toArray(function(events) {
+      expect(events).to.eql([
+        {
+          seq: '1',
+          type: 'logs',
+          host: 'foo',
+          source: 'bar',
+          message: '[2016-04-07T07:38:04.396Z] one\ntwo\nthree'
+        }
+      ]);
+
+      remaining.toArray(function(events) {
+        expect(events).to.eql([
+          [{
+            seq: '4',
+            type: 'logs',
+            host: 'foo',
+            source: 'bar',
+            message: '[2016-04-07T07:38:05.396Z] four'
+          }]
+        ]);
+
+        done();
+      });
+    });
+  });
+
+  it('supports forward merging', function(done) {
+    let remaining = _();
+
+    _([
+      {type: 'logs', host: 'foo', source: 'bar', message: 'foo \\'},
+      {type: 'logs', host: 'foo', source: 'bar', message: 'bar \\'},
+      {type: 'logs', host: 'foo', source: 'bar', message: 'baz'}
+    ])
+    .pipe(coalesce(remaining, {
+      pattern: '\\\\$',
+      what: 'next'
+    }))
+    .toArray(function(events) {
+      expect(events).to.eql([
+        {
+          type: 'logs',
+          host: 'foo',
+          source: 'bar',
+          message: 'foo \\\nbar \\\nbaz'
+        }
+      ]);
+
+      done();
+    });
+  });
+
+  it('handles multiple event streams', function(done) {
+    let remaining = _();
+
+    _([
+      {seq: 'a1', type: 'logs', host: 'foo', source: 'bar', message: 'a one'},
+      {seq: 'a2', type: 'logs', host: 'foo', source: 'bar', message: '  a two'},
+      {seq: 'b1', type: 'logs', host: 'foo', source: 'baz', message: 'b one'},
+      {seq: 'a3', type: 'logs', host: 'foo', source: 'bar', message: '  a three'},
+      {seq: 'b2', type: 'logs', host: 'foo', source: 'baz', message: '  b two'},
+      {seq: 'b3', type: 'logs', host: 'foo', source: 'baz', message: '  b three'},
+      {seq: 'a4', type: 'logs', host: 'foo', source: 'bar', message: 'a four'},
+      {seq: 'b4', type: 'logs', host: 'foo', source: 'baz', message: 'b four'}
+    ])
+    .pipe(coalesce(remaining, {
+      pattern: '^\\s+'
+    }))
+    .toArray(function(events) {
+      expect(events).to.eql([
+        {
+          seq: 'a1',
+          type: 'logs',
+          host: 'foo',
+          source: 'bar',
+          message: 'a one\n  a two\n  a three'
+        },
+        {
+          seq: 'b1',
+          type: 'logs',
+          host: 'foo',
+          source: 'baz',
+          message: 'b one\n  b two\n  b three'
+        }
+      ]);
+
+      remaining.toArray(function(events) {
+        expect(events).to.eql([
+          [{
+            seq: 'a4',
+            type: 'logs',
+            host: 'foo',
+            source: 'bar',
+            message: 'a four'
+          }],
+          [{
+            seq: 'b4',
+            type: 'logs',
+            host: 'foo',
+            source: 'baz',
+            message: 'b four'
+          }]
+        ]);
+
+        done();
+      });
+    });
+  });
+});
