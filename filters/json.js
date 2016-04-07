e6294b7e0707f18ed20c70def7157ade5342551b
protomouse <root@protomou.se>
2016-04-07 14:04:10 +0200

    json filter: add removeField option

--- filters/json.js
+++ filters/json.js
@@ -1,13 +1,15 @@
 'use strict';
 
-let _ = require('highland');
+let _ = require('highland')
+  , without = require('../lib/util').without;
 
 module.exports = function(opts) {
   opts = opts || {};
+  let removeField = opts.removeField || [];
 
   return _.pipeline(_.map(function(event) {
     try {
-      return _.extend(JSON.parse(event[opts.source]), _.extend(event, {}));
+      return without(_.extend(JSON.parse(event[opts.source]), _.extend(event, {})), removeField);
     } catch (err) {
       return event;
     }
