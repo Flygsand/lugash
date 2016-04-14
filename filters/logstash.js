4095720e92d3a393370404a6a06fc461fc388d33
protomouse <root@protomou.se>
2016-04-14 16:34:37 +0200

    logstash filter: support additional timestamp formats

--- filters/logstash.js
+++ filters/logstash.js
@@ -3,24 +3,43 @@
 var moment = require('moment')
   , _ = require('highland');
 
-function timestamp(event) {
+function validTimestamp(ts) {
+  return moment.isMoment(ts) && ts.isValid();
+}
+
+function timestamp(event, formats) {
+  var ts, fts;
+
   if (!event.timestamp) {
     throw new Error('missing timestamp');
   }
 
-  var ts = moment(new Date(event.timestamp));
-  if (!moment.isMoment(ts) || !ts.isValid()) {
-    throw new Error('timestamp is invalid');
+  for (var i in formats) {
+    fts = moment(event.timestamp, formats[i]);
+    if (validTimestamp(fts)) {
+      ts = fts;
+      break;
+    }
+  }
+
+  if (!ts) {
+    ts = moment(new Date(event.timestamp));
+    if (!validTimestamp(ts)) {
+      throw new Error('timestamp is invalid');
+    }
   }
 
   return ts.toISOString();
 }
 
-module.exports = function() {
+module.exports = function(opts) {
+  opts = opts || {};
+  var timestampFormats = opts.timestampFormats || [];
+
   return _.pipeline(_.map(function(event) {
     return _.extend(event, {
       '@version': '1',
-      '@timestamp': event['@timestamp'] || timestamp(event)
+      '@timestamp': event['@timestamp'] || timestamp(event, timestampFormats)
     });
   }));
 };
--- test/unit/filters/logstash_test.js
+++ test/unit/filters/logstash_test.js
@@ -9,15 +9,25 @@ describe('filters/logstash', function() {
     _([
       {
         timestamp: '2016-03-15T12:42:15+11:00'
+      },
+      {
+        timestamp: '13/Apr/2016:01:05:44 +0000'
       }
     ])
-    .pipe(logstash())
+    .pipe(logstash({
+      timestampFormats: ['DD/MMM/YYYY:HH:mm:ss Z']
+    }))
     .toArray(function(events) {
       expect(events).to.eql([
         {
           '@version': '1',
           '@timestamp': '2016-03-15T01:42:15.000Z',
           'timestamp': '2016-03-15T12:42:15+11:00'
+        },
+        {
+          '@version': '1',
+          '@timestamp': '2016-04-13T01:05:44.000Z',
+          'timestamp': '13/Apr/2016:01:05:44 +0000'
         }
       ]);
       done();
